9d7fd9dd202920c9aa1e1ff770e67dbf900138cc
dj90 <koi147@naver.com>
2014-03-28 15:11:34 +0900

    다시수정했다.

--- testprojeck/src/Test.java
+++ testprojeck/src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("¼öÁ¤Çß´Ù.");
+		System.out.println("ë‹¤ì‹œìˆ˜ì •í–ˆë‹¤.");
 
 	}
 
